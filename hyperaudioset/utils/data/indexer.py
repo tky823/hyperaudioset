b0c4a4fdc2548164c890e8cc033ad1f9293ddbfa
tky823 <tky823.main+github@gmail.com>
2025-03-09 15:38:52 +0900

    [src] Support recurrsive operation in Indexer

--- hyperaudioset/utils/data/indexer.py
+++ hyperaudioset/utils/data/indexer.py
@@ -12,8 +12,16 @@ class Indexer:
         self.name_to_index = name_to_index
 
     def __call__(self, name_or_names: str | list[str]) -> int | list[int]:
+        index = self._name_to_index(name_or_names)
+
+        return index
+
+    def __len__(self) -> int:
+        return len(self.name_to_index)
+
+    def _name_to_index(self, name_or_names: str | list[str]) -> int | list[int]:
         if isinstance(name_or_names, str):
-            index = self._name_to_index(name_or_names)
+            index = self.name_to_index[name_or_names]
 
             return index
         else:
@@ -24,11 +32,3 @@ class Indexer:
                 indices.append(index)
 
             return indices
-
-    def __len__(self) -> int:
-        return len(self.name_to_index)
-
-    def _name_to_index(self, name: str) -> int:
-        index = self.name_to_index[name]
-
-        return index
