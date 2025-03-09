4fa66dc9632ec08605327546b0090a6e44a70869
tky823 <tky823.main+github@gmail.com>
2025-03-09 22:18:02 +0900

    [src] Support initialization by PoincareEmbedding

--- hyperaudioset/models/poincare.py
+++ hyperaudioset/models/poincare.py
@@ -2,5 +2,20 @@ import torch.nn as nn
 
 
 class PoincareEmbedding(nn.Embedding):
-    def __init__(self, *args, **kwargs) -> None:
+    """Poincare embedding.
+
+    Args:
+        range (tuple, optional): Range of weight in initialization.
+            Default: ``(-0.0001, 0.0001)``.
+
+    """
+
+    def __init__(self, *args, range: tuple[float] | None = None, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+
+        if range is None:
+            range = (-0.0001, 0.0001)
+
+        _min, _max = range
+
+        self.weight.data.uniform_(_min, _max)
