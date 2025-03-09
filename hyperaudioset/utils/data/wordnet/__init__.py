60f2e7a01f45c5b4295475cd962e6a604f2709c2
tky823 <tky823.main+github@gmail.com>
2025-03-09 15:07:24 +0900

    [src] Add Mammal dataset

--- hyperaudioset/utils/data/wordnet/__init__.py
+++ hyperaudioset/utils/data/wordnet/__init__.py
@@ -1,5 +1,7 @@
 from ._download import load_mammal_name_to_index
+from .dataset import Mammal
 
 __all__ = [
+    "Mammal",
     "load_mammal_name_to_index",
 ]
---
+++ hyperaudioset/utils/data/wordnet/dataset.py
@@ -0,0 +1,111 @@
+import json
+import os
+from typing import Iterator
+
+import torch
+from torch.utils.data import IterableDataset
+
+from ._download import download_wordnet_hierarchy
+
+
+class Mammal(IterableDataset):
+    def __init__(
+        self,
+        num_neg_samples: int = 1,
+        length: int | None = None,
+        seed: int = 0,
+    ) -> None:
+        super().__init__()
+
+        from ... import hyperaudioset_cache_dir
+
+        wordnet_root = os.path.join(hyperaudioset_cache_dir, "data", "WordNet")
+
+        url = "https://github.com/tky823/Audyn/releases/download/v0.0.5/wordnet_mammal.json"
+
+        if wordnet_root:
+            os.makedirs(wordnet_root, exist_ok=True)
+
+        filename = os.path.basename(url)
+        path = os.path.join(wordnet_root, filename)
+        chunk_size = 8192
+
+        if not os.path.exists(path):
+            download_wordnet_hierarchy(url, path, chunk_size=chunk_size)
+
+        with open(path) as f:
+            hierarchy: list[dict[str, str]] = json.load(f)
+
+        tags = []
+        pair_list = []
+
+        for sample in hierarchy:
+            name = sample["name"]
+            tags.append(name)
+
+            for child_name in sample["child"]:
+                pair_list.append({"self": name, "child": child_name})
+
+        if length is None:
+            length = len(pair_list)
+
+        self.tags = tags
+        self.hierarchy = hierarchy
+        self.pair_list = pair_list
+
+        self.num_neg_samples = num_neg_samples
+        self.length = length
+
+        self.generator = None
+        self.seed = seed
+
+    def __iter__(self) -> Iterator[tuple[int, int, list[int]]]:
+        tags = self.tags
+        hierarchy = self.hierarchy
+        num_neg_samples = self.num_neg_samples
+        length = self.length
+        seed = self.seed
+
+        if self.generator is None:
+            self.generator = torch.Generator()
+            self.generator.manual_seed(seed)
+
+        indices = torch.randint(
+            0,
+            len(self.pair_list),
+            (length,),
+            generator=self.generator,
+        )
+        indices = indices.tolist()
+
+        for pair_index in indices:
+            pair = self.pair_list[pair_index]
+
+            if torch.rand((), generator=self.generator) < 0.5:
+                anchor = pair["self"]
+                positive = pair["child"]
+            else:
+                anchor = pair["child"]
+                positive = pair["self"]
+
+            anchor_index = tags.index(anchor)
+            parent = hierarchy[anchor_index]["parent"]
+            child = hierarchy[anchor_index]["child"]
+            positive_candidates = set(parent) | set(child)
+            negative_candidates = set(tags) - set(positive_candidates) - {anchor}
+
+            negative_indices = torch.randint(
+                0, len(negative_candidates), (num_neg_samples,)
+            )
+            negative_indices = negative_indices.tolist()
+
+            negative = []
+
+            for negative_index in negative_indices:
+                _negative = hierarchy[negative_index]["name"]
+                negative.append(_negative)
+
+            yield anchor, positive, negative
+
+    def __len__(self) -> int:
+        return self.length
