309467df7c8a75a48fbaade27bcf1434c44b8514
tky823 <tky823.main+github@gmail.com>
2025-03-10 02:14:02 +0900

    [src] Define index_to_name in Indexer

--- hyperaudioset/utils/data/indexer.py
+++ hyperaudioset/utils/data/indexer.py
@@ -20,15 +20,33 @@ class Indexer:
     def __init__(self, name_to_index: dict[str, int]) -> None:
         super().__init__()
 
+        index_to_name = {}
+
+        for name, index in name_to_index.items():
+            assert index not in index_to_name
+
+            index_to_name[index] = name
+
         self.name_to_index = name_to_index
+        self.index_to_name = index_to_name
 
     def __call__(self, name_or_names: str | list[str]) -> int | list[int]:
-        index = self._name_to_index(name_or_names)
+        index = self.encode(name_or_names)
 
         return index
 
     def __len__(self) -> int:
         return len(self.name_to_index)
+
+    def encode(self, name_or_names: str | list[str]) -> int | list[int]:
+        index = self._name_to_index(name_or_names)
+
+        return index
+
+    def decode(self, index_or_indices: int | list[int]) -> str | list[str]:
+        name = self._index_to_name(index_or_indices)
+
+        return name
 
     def _name_to_index(self, name_or_names: str | list[str]) -> int | list[int]:
         if isinstance(name_or_names, str):
@@ -43,3 +61,17 @@ class Indexer:
                 indices.append(index)
 
             return indices
+
+    def _index_to_name(self, index_or_indices: int | list[int]) -> str | list[str]:
+        if isinstance(index_or_indices, int):
+            name = self.index_to_name[index_or_indices]
+
+            return name
+        else:
+            names = []
+
+            for index in index_or_indices:
+                name = self._index_to_name(index)
+                names.append(name)
+
+            return names
