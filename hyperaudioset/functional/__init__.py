c94e1b4326c45f7bbad9dd42d8fff25db0f24e2e
tky823 <tky823.main+github@gmail.com>
2025-03-09 22:40:53 +0900

    [src] Add mobius_add

---
+++ hyperaudioset/functional/__init__.py
@@ -0,0 +1,5 @@
+from .hyperbolic import mobius_add
+
+__all__ = [
+    "mobius_add",
+]
---
+++ hyperaudioset/functional/hyperbolic.py
@@ -0,0 +1,44 @@
+import torch
+
+
+def mobius_add(
+    input: torch.Tensor, other: torch.Tensor, curvature: torch.Tensor | float = 1
+) -> torch.Tensor:
+    if input.size() != other.size():
+        dim = max(input.dim(), other.dim())
+
+        missing_dim = dim - input.dim()
+        missing_shape = (1,) * missing_dim
+        target_shape = missing_shape + input.size()
+        input = input.view(*target_shape)
+
+        missing_dim = dim - other.dim()
+        missing_shape = (1,) * missing_dim
+        target_shape = missing_shape + other.size()
+        other = other.view(*target_shape)
+
+        target_shape = ()
+
+        for _input_size, _other_size in zip(input.size(), other.size()):
+            target_shape = target_shape + (max(_input_size, _other_size),)
+
+        input = input.expand(target_shape).contiguous()
+        other = other.expand(target_shape).contiguous()
+
+    *batch_shape, num_features = input.size()
+
+    input = input.view(-1, num_features)
+    other = other.view(-1, num_features)
+
+    dot = torch.sum(input * other, dim=-1)
+    norm_input = torch.sum(input**2, dim=-1)
+    norm_other = torch.sum(other**2, dim=-1)
+
+    coeff_input = 1 + 2 * curvature * dot + curvature * norm_other
+    coeff_other = 1 - curvature * norm_input
+    denom = 1 + 2 * curvature * dot + curvature * norm_input * norm_other
+    num = coeff_input.unsqueeze(dim=-1) * input + coeff_other.unsqueeze(dim=-1) * other
+    output = num / denom.unsqueeze(dim=-1)
+    output = output.view(*batch_shape, num_features)
+
+    return output
