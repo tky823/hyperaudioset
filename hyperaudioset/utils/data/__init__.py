325c1fb22fd9eae82de7a4492de01b64e6842cf9
tky823 <tky823.main+github@gmail.com>
2025-03-09 15:34:49 +0900

    [src] Add collator for negative sampling

--- hyperaudioset/utils/data/__init__.py
+++ hyperaudioset/utils/data/__init__.py
@@ -1,5 +1,27 @@
+from typing import Any
+
 from .indexer import Indexer
 
 __all__ = [
     "Indexer",
+    "NegativeSamplingCollator",
 ]
+
+
+class NegativeSamplingCollator:
+    def __init__(self) -> None:
+        pass
+
+    def __call__(
+        self, list_batch: list[tuple[str, str, list[str]]]
+    ) -> tuple[list[str], list[str], list[list[str]]]:
+        anchors = []
+        positives = []
+        negatives = []
+
+        for anchor, positive, negative in list_batch:
+            anchors.append(anchor)
+            positives.append(positive)
+            negatives.append(negative)
+
+        return anchors, positives, negatives
