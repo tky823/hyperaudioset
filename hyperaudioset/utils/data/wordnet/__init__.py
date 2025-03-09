27c421d04b8eea574b7c68f199b4379ee8ee09cd
tky823 <tky823.main+github@gmail.com>
2025-03-09 15:02:55 +0900

    [src] Add utils for WordNet

---
+++ hyperaudioset/utils/data/wordnet/__init__.py
@@ -0,0 +1,5 @@
+from ._download import load_mammal_name_to_index
+
+__all__ = [
+    "load_mammal_name_to_index",
+]
---
+++ hyperaudioset/utils/data/wordnet/_download.py
@@ -0,0 +1,50 @@
+import json
+import os
+import shutil
+import uuid
+
+from ..download import download_file
+
+
+def load_mammal_name_to_index(chunk_size: int = 8192) -> dict[str, int]:
+    from ... import hyperaudioset_cache_dir
+
+    wordnet_root = os.path.join(hyperaudioset_cache_dir, "data", "WordNet")
+
+    url = "https://github.com/tky823/Audyn/releases/download/v0.0.5/wordnet_mammal.json"
+
+    if chunk_size is None:
+        chunk_size = 8192
+
+    if wordnet_root:
+        os.makedirs(wordnet_root, exist_ok=True)
+
+    filename = os.path.basename(url)
+    path = os.path.join(wordnet_root, filename)
+
+    if not os.path.exists(path):
+        download_wordnet_hierarchy(url, path, chunk_size=chunk_size)
+
+    with open(path) as f:
+        hierarchy: list[dict[str, str]] = json.load(f)
+
+    name_to_index = {}
+
+    for index, sample in enumerate(hierarchy):
+        name = sample["name"]
+        name_to_index[name] = index
+
+    return name_to_index
+
+
+def download_wordnet_hierarchy(url: str, path: str, chunk_size: int = 8192) -> None:
+    temp_path = path + str(uuid.uuid4())[:8]
+
+    try:
+        download_file(url, temp_path, chunk_size=chunk_size)
+        shutil.move(temp_path, path)
+    except (Exception, KeyboardInterrupt) as e:
+        if os.path.exists(temp_path):
+            os.remove(temp_path)
+
+        raise e
