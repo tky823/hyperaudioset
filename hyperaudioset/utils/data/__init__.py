27adedd32aa132c0c0b4a5326a923871357fa439
tky823 <tky823.main+github@gmail.com>
2025-03-09 14:58:26 +0900

    [src] Define indexer

---
+++ hyperaudioset/utils/data/__init__.py
@@ -0,0 +1,5 @@
+from .indexer import Indexer
+
+__all__ = [
+    "Indexer",
+]
---
+++ hyperaudioset/utils/data/indexer.py
@@ -0,0 +1,31 @@
+class Indexer:
+    """Indexer to map name into index.
+
+    Args:
+        name_to_index (dict): Dictionary to map name to index.
+
+    """
+
+    def __init__(self, name_to_index: dict[str, int]) -> None:
+        super().__init__()
+
+        self.name_to_index = name_to_index
+
+    def __call__(self, name_or_names: str | list[str]) -> int | list[int]:
+        if isinstance(name_or_names, str):
+            index = self._name_to_index(name_or_names)
+
+            return index
+        else:
+            indices = []
+
+            for name in name_or_names:
+                index = self._name_to_index(name)
+                indices.append(index)
+
+            return indices
+
+    def _name_to_index(self, name: str) -> int:
+        index = self.name_to_index[name]
+
+        return index
