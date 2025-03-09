97393510d97236190d697d1e9ce89831c72cc655
tky823 <tky823.main+github@gmail.com>
2025-03-09 15:03:16 +0900

    [src] Support len() by Indexer

--- hyperaudioset/utils/data/indexer.py
+++ hyperaudioset/utils/data/indexer.py
@@ -25,6 +25,9 @@ class Indexer:
 
             return indices
 
+    def __len__(self) -> int:
+        return len(self.name_to_index)
+
     def _name_to_index(self, name: str) -> int:
         index = self.name_to_index[name]
 
