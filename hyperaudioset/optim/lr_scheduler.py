7297e7b78c913708948a33a16729472c423bbe91
tky823 <tky823.main+github@gmail.com>
2025-03-10 01:13:29 +0900

    [src] Add BurnInLRScheduler

--- hyperaudioset/optim/lr_scheduler.py
+++ hyperaudioset/optim/lr_scheduler.py
@@ -1,8 +1,9 @@
 from torch.optim import Optimizer
-from torch.optim.lr_scheduler import _LRScheduler
+from torch.optim.lr_scheduler import LambdaLR, _LRScheduler
 
 __all__ = [
     "DummyLRScheduler",
+    "BurnInLRScheduler",
 ]
 
 
@@ -12,3 +13,16 @@ class DummyLRScheduler(_LRScheduler):
 
     def step(self, *args, **kwargs) -> None:
         pass
+
+
+class BurnInLRScheduler(LambdaLR):
+    def __init__(
+        self, optimizer: Optimizer, burnin_step: int, burnin_scale: float, **kwargs
+    ) -> None:
+        def lr_lambda(epoch: int) -> float:
+            if epoch < burnin_step:
+                return burnin_scale
+            else:
+                return 1
+
+        super().__init__(optimizer, lr_lambda, **kwargs)
