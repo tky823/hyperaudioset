8ee76ad2474cae25e3ad13d5dc69e2e59ba7b4df
tky823 <tky823.main+github@gmail.com>
2025-03-10 23:21:22 +0900

    [src] Support curvature

--- hyperaudioset/models/poincare.py
+++ hyperaudioset/models/poincare.py
@@ -1,3 +1,4 @@
+import math
 from typing import Any
 
 import torch
@@ -10,39 +11,57 @@ class PoincareEmbedding(ManifoldEmbedding):
     """Poincare embedding.
 
     Args:
+        curvature (float): Nonnegative curvature.
         range (tuple, optional): Range of weight in initialization.
             Default: ``(-0.0001, 0.0001)``.
 
     """
 
     def __init__(
-        self, *args, range: tuple[float] | None = None, eps: float = 1e-3, **kwargs
+        self,
+        *args,
+        curvature: float = 1,
+        range: tuple[float] | None = None,
+        eps: float = 1e-3,
+        **kwargs,
     ) -> None:
         super().__init__(*args, **kwargs)
 
+        assert curvature > 0, "curvature should be positive."
+
+        self.curvature = curvature
         self.eps = eps
 
         self._reset_parameters(range)
 
     def _reset_parameters(self, range: tuple[float] | None = None) -> None:
+        curvature = self.curvature
+
         if range is None:
             range = (-0.0001, 0.0001)
 
         _min, _max = range
 
+        assert -1 / math.sqrt(curvature) < _min
+        assert _max < 1 / math.sqrt(curvature)
+
         self.weight.data.uniform_(_min, _max)
 
     def sub(self, input: torch.Tensor, other: torch.Tensor) -> torch.Tensor:
-        return mobius_add(input, -other)
+        curvature = self.curvature
+
+        return mobius_add(input, -other, curvature=curvature)
 
     def proj(self, embedding: torch.Tensor) -> torch.Tensor:
+        curvature = self.curvature
         eps = self.eps
 
         assert embedding.dim() == 2
 
+        allowed_norm = 1 / math.sqrt(curvature) - eps
         norm = torch.linalg.vector_norm(embedding, dim=-1, keepdim=True)
-        projected_embedding = (1 - eps) * embedding / norm
-        condition = norm > 1 - self.eps
+        projected_embedding = allowed_norm * embedding / norm
+        condition = norm > allowed_norm
         embedding = torch.where(condition, projected_embedding, embedding)
 
         return embedding
