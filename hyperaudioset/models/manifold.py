6429a9207f1e622e16129dea3ce065e07efa3ae7
tky823 <tky823.main+github@gmail.com>
2025-05-03 17:16:43 +0900

    [src] Refine implementation of manifolds

--- hyperaudioset/models/manifold.py
+++ hyperaudioset/models/manifold.py
@@ -1,11 +1,37 @@
 from abc import abstractmethod
+from typing import Union
 
 import torch
 import torch.nn as nn
 
 
 class ManifoldEmbedding(nn.Embedding):
+    """Base class of manifold embedding."""
+
+    @abstractmethod
+    def add(self, input: torch.Tensor, other: torch.Tensor) -> torch.Tensor:
+        """Add other to input."""
+
     @abstractmethod
     def sub(self, input: torch.Tensor, other: torch.Tensor) -> torch.Tensor:
         """Subtract other from input."""
-        pass
+
+    def retmap(
+        self, input: torch.Tensor, root: Union[torch.Tensor, float] = 0
+    ) -> torch.Tensor:
+        """Retraction map, which is a first-order of approximation of exponential map."""
+        output = input + root
+
+        return output
+
+    def expmap(
+        self, input: torch.Tensor, root: Union[torch.Tensor, float] = 0
+    ) -> torch.Tensor:
+        """Exponential map, which can be approximated by retraction map."""
+        output = self.retmap(input, root=root)
+
+        return output
+
+    @abstractmethod
+    def proj(self, input: torch.Tensor) -> torch.Tensor:
+        """Projection, which ensures input should be on manifold."""
--- hyperaudioset/models/poincare.py
+++ hyperaudioset/models/poincare.py
@@ -1,8 +1,8 @@
-from typing import Any
+from typing import Any, Optional, Union
 
 import torch
 
-from ..functional.hyperbolic import mobius_sub
+from ..functional.hyperbolic import mobius_add, mobius_sub
 from .manifold import ManifoldEmbedding
 
 
@@ -10,7 +10,8 @@ class PoincareEmbedding(ManifoldEmbedding):
     """Poincare embedding.
 
     Args:
-        radius (float): Radius of Poincare ball. Default: ``1``.
+        curvature (float): Negative curvature of Poincare ball. Default: ``-1``.
+        dim (int): Dimension of Poincare ball. Default: ``-1``.
         range (tuple, optional): Range of weight in initialization.
             Default: ``(-0.001, 0.001)``.
 
@@ -19,26 +20,29 @@ class PoincareEmbedding(ManifoldEmbedding):
     def __init__(
         self,
         *args,
-        radius: float = 1,
-        range: tuple[float] | None = None,
+        curvature: float = -1,
+        dim: int = -1,
+        range: Optional[tuple[float]] = None,
         eps: float = 1e-5,
         **kwargs,
     ) -> None:
         super().__init__(*args, **kwargs)
 
-        assert radius > 0, "radius should be positive."
+        assert curvature < 0, "curvature should be negative."
 
-        self.radius = radius
+        self.dim = dim
+        self.curvature = curvature
         self.eps = eps
 
         self._reset_parameters(range)
 
-    def _reset_parameters(self, range: tuple[float] | None = None) -> None:
-        radius = self.radius
+    def _reset_parameters(self, range: Optional[tuple[float]] = None) -> None:
+        curvature = self.curvature
 
         if range is None:
             range = (-0.001, 0.001)
 
+        radius = 1 / ((-curvature) ** 0.5)
         _min, _max = range
 
         assert -radius < _min, f"Minimum ({_min}) should be greater than {-radius}."
@@ -46,68 +50,95 @@ class PoincareEmbedding(ManifoldEmbedding):
 
         self.weight.data.uniform_(_min, _max)
 
-    def sub(self, input: torch.Tensor, other: torch.Tensor) -> torch.Tensor:
-        radius = self.radius
+    def add(self, input: torch.Tensor, other: torch.Tensor) -> torch.Tensor:
+        curvature = self.curvature
+        dim = self.dim
         eps = self.eps
 
-        return mobius_sub(input, other, radius=radius, eps=eps)
+        return mobius_add(input, other, curvature=curvature, dim=dim, eps=eps)
 
-    def retmap(
-        self, input: torch.Tensor, point: torch.Tensor | float = 0
-    ) -> torch.Tensor:
-        """Retraction map, which is a first-order of approximation of exponential map."""
-        output = input + point
+    def sub(self, input: torch.Tensor, other: torch.Tensor) -> torch.Tensor:
+        curvature = self.curvature
+        dim = self.dim
+        eps = self.eps
 
-        return output
+        return mobius_sub(input, other, curvature=curvature, dim=dim, eps=eps)
 
     def expmap(
-        self, input: torch.Tensor, point: torch.Tensor | float = 0
+        self, input: torch.Tensor, root: Union[torch.Tensor, float] = 0
     ) -> torch.Tensor:
-        """Exponential map, which is approximated by retraction map here."""
-        output = self.retmap(input, point=point)
+        """Exponential map, which can be approximated by retraction map."""
+        curvature = self.curvature
+        dim = self.dim
+        eps = self.eps
+
+        _curvature = (-curvature) ** 0.5
+        norm = torch.linalg.vector_norm(input, dim=dim, keepdim=True)
+        norm = torch.clamp(norm, min=eps)
+        normalized_input = input / norm
+        conformal_factor = 2 / (
+            1 + curvature * torch.sum(input**2, dim=dim, keepdim=True)
+        )
+        y = (
+            torch.tanh(_curvature * conformal_factor * norm / 2)
+            * normalized_input
+            / _curvature
+        )
+        output = mobius_add(root, y, curvature=curvature, dim=dim, eps=eps)
 
         return output
 
     def proj(self, input: torch.Tensor) -> torch.Tensor:
         """Projection, which ensures input should be on Poincare disk."""
-        radius = self.radius
+        curvature = self.curvature
+        dim = self.dim
         eps = self.eps
 
+        assert dim == -1, "Only dim = -1 is supported"
+
         *batch_shape, embedding_dim = input.size()
-        maxnorm = radius - eps
+
+        radius = 1 / ((-curvature) ** 0.5)
 
         x = input.view(-1, embedding_dim)
-        x = torch.renorm(x, p=2, dim=0, maxnorm=maxnorm)
+        x = torch.renorm(x, p=2, dim=0, maxnorm=radius - eps)
         output = x.view(*batch_shape, embedding_dim)
 
         return output
 
     def forward(self, input: torch.Tensor) -> torch.Tensor:
-        radius = self.radius
+        curvature = self.curvature
+        dim = self.dim
 
         x = super().forward(input)
-        output = RiemannGradientFunction.apply(x, radius)
+        output = PoincareGradientFunction.apply(x, curvature, dim)
 
         return output
 
 
-class RiemannGradientFunction(torch.autograd.Function):
+class PoincareGradientFunction(torch.autograd.Function):
     @staticmethod
-    def forward(ctx: Any, input: torch.Tensor, radius: float) -> torch.Tensor:
-        assert not isinstance(radius, torch.Tensor)
+    def forward(
+        ctx: Any, input: torch.Tensor, curvature: float, dim: int
+    ) -> torch.Tensor:
+        assert not isinstance(curvature, torch.Tensor)
 
         ctx.save_for_backward(input)
-        ctx.radius = radius
+        ctx.curvature = curvature
+        ctx.dim = dim
 
         return input
 
     @staticmethod
     def backward(ctx: Any, grad_output: torch.Tensor) -> torch.Tensor:
         (input,) = ctx.saved_tensors
-        radius = ctx.radius
+        curvature = ctx.curvature
+        dim = ctx.dim
 
-        num = 1 - torch.sum(input**2, dim=-1) / (radius**2)
-        scale = (num**2) / 4
-        grad_input = scale.unsqueeze(dim=-1) * grad_output
+        assert dim == -1, "Only dim = -1 is supported."
 
-        return grad_input, None
+        conformal_factor = 2 / (1 + curvature * torch.sum(input**2, dim=dim))
+        scale = 1 / (conformal_factor**2)
+        grad_input = scale.unsqueeze(dim=dim) * grad_output
+
+        return grad_input, None, None
